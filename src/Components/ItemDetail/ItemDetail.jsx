474fd091bd6f1c94b30afbd6dcfeb671485d604f
Erika <55201104+ErikaZimmer@users.noreply.github.com>
2022-04-19 14:43:05 -0300

    Add files via upload

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import ItemCount from "../ItemCount";
 import "./ItemDetail.css";
+import ItemCount from "../ItemCount";
+import { useState } from "react";
 
 function ItemDetail(product){
+
+  const [carrito, setCarrito] = useState(1);
+
+    function onAdd(dato){
+        setCarrito(dato);
+    }
+    console.log(carrito);
+
     return (
       <>
-        <div className="detailContainer">
+        <div className="detailContainer" >
           <img
             className="d-block detailImage"
             src={product.product.img}
@@ -14,7 +23,7 @@ function ItemDetail(product){
           <div>
             <h3>{product.product.name}</h3>
             <p className="detailPrice"> $ {product.product.price}</p>
-            <ItemCount/>
+            <ItemCount stock={product.product.stock} initial={1} onAdd={onAdd}/>
           </div>
         </div>
       </>
